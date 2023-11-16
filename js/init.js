5d02829f4e44dd64d72f8afd3df690eda9e76149
prufirebase2023 <126984448+prufirebase2023@users.noreply.github.com>
2023-11-16 10:15:19 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyBqOpaywbPvN6xZeVvPykxZsxkz9UYf0DE",
+    authDomain: "base43p.firebaseapp.com",
+    projectId: "base43p",
+    storageBucket: "base43p.appspot.com",
+    messagingSenderId: "1005116666609",
+    appId: "1:1005116666609:web:0100ca2594b6c55ac4ef50",
+    measurementId: "G-6N5T55PF16"
 });
